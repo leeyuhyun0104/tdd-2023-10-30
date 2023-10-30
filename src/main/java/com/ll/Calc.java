7bf74adc39491419b9d485ad10be9f0236a34956
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-10-30 10:41:38 +0900

    에러 해결

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,7 +1,9 @@
 package com.ll;
 
 class Calc {
-    public static void run(String s) {
+    public static int run(String s) {
 
+        return 0;
+        return 20;
     }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -5,8 +5,16 @@ import org.junit.jupiter.api.Test;
 
 public class CalcTest {
     @Test
-    @DisplayName("Clac에 run 메서드는 int를 리턴한다.");
-    void t(){
+    @DisplayName("Calc에 run 메서드는 int를 리턴한다.")
+    void t1(){
         int rs = Calc.run("");
+    }
+
+    @Test
+    @DisplayName("10+10=20")
+    void t2(){
+        int rs = Calc.run("10+10");
+
+        assertThat(rs).isEqualTo(20);
     }
 }
